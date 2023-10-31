de6a4a351d7dc98b10cbe54e0a18c1dd6d3ec65d
kjhonggg95 <kjhonggg95@gmail.com>
2023-10-31 22:54:49 +0900

    init: test

---
+++ kjhonggg95/boj_template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define X first
+#define Y second
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int main()
+{
+    fastio;
+    
+}
