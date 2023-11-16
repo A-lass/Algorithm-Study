cbee6689d6e1cce20aa665e8bfe2b8c953a4f29f
sunki <96693975+qsunki@users.noreply.github.com>
2023-11-16 15:22:46 +0900

    ajsthfldu -> qsunki

--- script/update_readme.py
+++ script/update_readme.py
@@ -5,7 +5,7 @@ import os
 GH_TOKEN = os.environ.get('GH_TOKEN')
 
 # 유저, 디렉토리 설정
-users = ["ddingmin", "kjhonggg95", "ajsthfldu", "dydwo0740"]
+users = ["ddingmin", "kjhonggg95", "qsunki", "dydwo0740"]
 dirs = ["baekjoon"]
 
 url = f"https://api.github.com/repos/{os.environ.get('GITHUB_REPOSITORY')}/contents/"
