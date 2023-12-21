150841b6acd44ab650ce704870c618b6655f5c27
sunki <ajsthfldu@outlook.com>
2023-12-21 20:17:20 +0900

    웹훅 포함

--- .github/get_problem/main.js
+++ .github/get_problem/main.js
@@ -16,34 +16,51 @@ async function run() {
 
 
     console.log(`Content: ${elementContent}`);
-      // 날짜
-      const today = new Date();
-  
-      // UTC+9 설정
-      const options = { timeZone: 'Asia/Seoul', year: 'numeric', month: '2-digit', day: '2-digit' };
-      const dateFormatter = new Intl.DateTimeFormat('ko-KR', options);
-  
-      // 날짜를 문자열로 변환
-      const formattedDate = dateFormatter.format(today);
-  
-      // GitHub 이슈 생성
-      const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-  
-      // GITHUB_REPOSITORY 환경 변수에서 owner와 repo 정보 추출
-      const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-  
-      // GitHub 이슈에 직접 문자열 전달
-      const issueResponse = await octokit.request('POST /repos/:owner/:repo/issues', {
-        owner,
-        repo,
-        title: formattedDate,
-        body: `https://www.acmicpc.net/problem/${elementContent}`,
+    // 날짜
+    const today = new Date();
+
+    // UTC+9 설정
+    const options = { timeZone: 'Asia/Seoul', year: 'numeric', month: '2-digit', day: '2-digit' };
+    const dateFormatter = new Intl.DateTimeFormat('ko-KR', options);
+
+    // 날짜를 문자열로 변환
+    const formattedDate = dateFormatter.format(today);
+
+    // GitHub 이슈 생성
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+    // GITHUB_REPOSITORY 환경 변수에서 owner와 repo 정보 추출
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+
+    // GitHub 이슈에 직접 문자열 전달
+    const issueResponse = await octokit.request('POST /repos/:owner/:repo/issues', {
+      owner,
+      repo,
+      title: formattedDate,
+      body: `https://www.acmicpc.net/problem/${elementContent}`,
+    });
+
+    console.log('Issue created:', issueResponse.data.html_url);
+
+    // 웹훅에 메세지 보내기
+    const webhookUrl = process.env.DISCORD_ALGO;
+
+    const message = {
+      content: `${formattedDate}\nhttps://www.acmicpc.net/problem/${elementContent}`,
+      username: '오늘의 알고리즘', // 앱 이름 설정
+      // avatar_url: 'https://example.com/your-avatar-image.png', // 아이콘 URL 설정
+    };
+
+    axios.post(webhookUrl, message)
+      .then(response => {
+        console.log('메시지가 성공적으로 전송되었습니다:', response.data);
+      })
+      .catch(error => {
+        console.error('메시지 전송 중 오류가 발생했습니다:', error.response.data);
       });
-  
-      console.log('Issue created:', issueResponse.data.html_url);
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
 
 run();
