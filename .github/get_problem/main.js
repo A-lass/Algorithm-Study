b938302075fd8daba9d7039c27820794ea409257
qsunki <ajsthfldu@outlook.com>
2024-06-19 15:32:56 +0900

    Update get_problem script

--- .github/get_problem/main.js
+++ .github/get_problem/main.js
@@ -1,73 +1,57 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { Octokit } = require('@octokit/core');
+import axios from "axios";
+import {Octokit} from "octokit";
+import * as cheerio from "cheerio";
 
 async function getProblemUrl() {
-  try {
-    const response = await axios.get('https://solved.ac/search?query=*g5..g3+s%23100..+%21%23math+%21%23geometry+%21%23number_theory+%21%40kjhonggg+%21%40ddingmin+%21%40dydwo0740+%21%40inbloom+%21%40pon06061&sort=random&direction=asc&page=1');
+    const response = await axios.get('https://solved.ac/search?query=*g3..p5+s%23200..+%21%23math+%21%23geometry+%21%23number_theory+%21%40dydwo0740+%21%40inbloom+w%3Ffalse+%25ko&sort=random&direction=asc&page=1');
     const html = response.data;
     const $ = cheerio.load(html);
+    const problemId = $('#__next > div.css-1shh4uc > div:nth-child(4) > div.css-qijqp5 > table > tbody > tr:nth-child(2) > td:nth-child(1) > div > div > div > span > span > a').text();
+    if (!problemId) {
+        throw new Error('Failed to fetch the problem URL');
+    }
 
-    const elementContent = $('#__next > div > div:nth-child(4) > div.css-qijqp5 > table > tbody > tr:nth-child(2) > td:nth-child(1) > div > div > div > span > span > a').text();
-
-    return `https://www.acmicpc.net/problem/${elementContent}`;
-  } catch (error) {
-    throw new Error('Failed to fetch the problem URL');
-  }
+    console.log('Got problem id: ', problemId)
+    return `https://www.acmicpc.net/problem/${problemId}`;
 }
 
 async function createGitHubIssue(url) {
-  try {
-    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+    const octokit = new Octokit({auth: process.env.GITHUB_TOKEN});
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
     const today = new Date();
 
-    const options = { timeZone: 'Asia/Seoul', year: 'numeric', month: '2-digit', day: '2-digit' };
+    const options = {timeZone: 'Asia/Seoul', year: 'numeric', month: '2-digit', day: '2-digit'};
     const dateFormatter = new Intl.DateTimeFormat('ko-KR', options);
     const formattedDate = dateFormatter.format(today);
 
     const issueResponse = await octokit.request('POST /repos/:owner/:repo/issues', {
-      owner,
-      repo,
-      title: formattedDate,
-      body: url,
+        owner,
+        repo,
+        title: formattedDate,
+        body: url,
     });
-
     console.log('GitHub Issue created:', issueResponse.data.html_url);
-  } catch (error) {
-    console.error('Error creating GitHub Issue:', error.message);
-  }
 }
 
 async function sendDiscordMessage(messageContent) {
-  try {
     const webhookUrl = process.env.DISCORD_ALGO;
     const message = {
-      content: messageContent,
-      username: '오늘의 알고리즘',
+        content: messageContent,
+        username: '오늘의 알고리즘',
     };
 
     const response = await axios.post(webhookUrl, message);
     console.log('Discord message sent successfully:', response.data);
-  } catch (error) {
-    console.error('Error sending Discord message:', error.message);
-  }
 }
 
-async function run() {
-  try {
-    const problemUrl = await getProblemUrl();
-    const formattedDate = new Intl.DateTimeFormat('ko-KR', { timeZone: 'Asia/Seoul', year: 'numeric', month: '2-digit', day: '2-digit' }).format(new Date());
-    
-    // GitHub Issue 생성
-    await createGitHubIssue(problemUrl);
+const formattedDate = new Intl.DateTimeFormat('ko-KR', {
+    timeZone: 'Asia/Seoul',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+}).format(new Date());
 
-    // Discord 메시지 전송
-    const discordMessageContent = `${formattedDate}\n${problemUrl}`;
-    await sendDiscordMessage(discordMessageContent);
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-}
-
-run();
+const problemUrl = await getProblemUrl();
+console.log(problemUrl)
+await createGitHubIssue(problemUrl);
+await sendDiscordMessage(`${formattedDate}\n${problemUrl}`);
